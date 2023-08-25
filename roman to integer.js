27fe401834b1789f3705feb6c0baeb66568383f8
Md Rashidul Haq <77320958+Rashidulhaq@users.noreply.github.com>
2023-08-26 00:44:39 +0600

    Add files via upload

---
+++ roman to integer.js
@@ -0,0 +1,29 @@
+
+var romanToInt = function (s) {
+const map = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+    let result = 0;
+    let prev = null;
+
+    for(let i = 0; i < s.length; i++) {
+        if(prev && prev < map[s[i]]) {
+            result = result - prev - prev;
+        }
+
+        prev = map[s[i]];
+        result += map[s[i]];
+    }
+
+    return result;
+};
+console.log(romanToInt('III'));
+console.log(romanToInt('LVIII'));
+console.log(romanToInt('MCMXCIV'));
