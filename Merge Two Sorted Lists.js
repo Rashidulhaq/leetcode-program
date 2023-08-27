72afed421897b89b56ef2d8bec88b18fada0bec6
Md Rashidul Haq <77320958+Rashidulhaq@users.noreply.github.com>
2023-08-27 07:17:28 +0600

    Update Merge Two Sorted Lists.js

--- Merge Two Sorted Lists.js
+++ Merge Two Sorted Lists.js
@@ -30,4 +30,6 @@ var mergeTwoLists = function (list1, list2) {
 
 const list1 = [1, 2, 4];
 const list2 = [1, 3, 4];
+const list1 = [];
+const list2 = [0];
 console.log(mergeTwoLists(list1, list2));
