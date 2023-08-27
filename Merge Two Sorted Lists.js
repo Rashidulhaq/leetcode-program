fcf6bb9db9650994e3bc00f3614d2f773e7468a4
Md Rashidul Haq <77320958+Rashidulhaq@users.noreply.github.com>
2023-08-27 07:16:05 +0600

    Add files via upload

---
+++ Merge Two Sorted Lists.js
@@ -0,0 +1,33 @@
+
+var mergeTwoLists = function (list1, list2) {
+    let result = [];
+    let i = 0, j = 0;
+
+    while (i < list1.length && j < list2.length) {
+        if (list1[i] <= list2[j]) {
+            result.push(list1[i]);
+            i++;
+        } else {
+            result.push(list2[j]);
+            j++;
+        }
+    }
+
+    // Add the remaining elements from list1 (if any)
+    while (i < list1.length) {
+        result.push(list1[i]);
+        i++;
+    }
+
+    // Add the remaining elements from list2 (if any)
+    while (j < list2.length) {
+        result.push(list2[j]);
+        j++;
+    }
+
+    return result;
+};
+
+const list1 = [1, 2, 4];
+const list2 = [1, 3, 4];
+console.log(mergeTwoLists(list1, list2));
